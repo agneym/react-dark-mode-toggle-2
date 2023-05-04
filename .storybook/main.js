32e88eed26f17ed22184c0a4744f51e66269f0b2
Agney Menon <agney@circle.co>
2023-05-05 03:24:15 +0530

    update deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
 module.exports = {
-  features: { postcss: false },
+  features: {
+    postcss: false
+  },
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-actions",
-  ],
-  framework: "@storybook/react",
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-actions"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
